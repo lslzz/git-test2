b71009b3f3228e588ae5982a7f4fb8f0dcbfba19
lzz <957362328@qq.com>
2022-10-11 11:25:37 +0800

    My first commit by idea.

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author lslzz
+ * @Date 2022/10/11 10:34
+ * @Version 1.0
+ **/
+public class GitTest  {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+    }
+}
