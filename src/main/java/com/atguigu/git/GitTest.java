e683ddb3d3f5cde1952ec764a5fdaeccd4941463
lzz <957362328@qq.com>
2022-10-11 15:19:59 +0800

    My second commit by idea with master.

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,17 +1,13 @@
 package com.atguigu.git;
 
-/**
- * @ClassName GitTest
- * @Description TODO
- * @Author lslzz
- * @Date 2022/10/11 10:34
- * @Version 1.0
- **/
+
 public class GitTest  {
     public static void main(String[] args) {
         System.out.println("hello git !");
         System.out.println("hello git01 !");
         System.out.println("hello git02 !");
         System.out.println("hello git03 !");
+        System.out.println("hello git04 by hot-fix !");
+        System.out.println("hello git05 by master !");
     }
 }
